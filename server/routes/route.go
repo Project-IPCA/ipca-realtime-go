cfc64e608a0f05bbd40c7363260aaa0d9216c3b5
ootsirasit <ootsirasit@gmail.com>
2024-12-15 16:27:53 +0700

    🎨 [APP-0003] Edit chapter permission to group permission and user connection logic

--- server/routes/route.go
+++ server/routes/route.go
@@ -7,14 +7,13 @@ import (
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
-func ConfigureRoutes(server *s.Server){
-	chapterPermissionHandler := handlers.NewChapterPermissionHandler(server)
+func ConfigureRoutes(server *s.Server) {
+	groupPermissionHandler := handlers.NewGroupPermissionHandler(server)
 	classLogHandler := handlers.NewClassLogHandler(server)
 	onlineStudentsOldHandler := handlers.NewOnlineStudentsHandler(server)
 	submitionResultHandler := handlers.NewSubmitionResultHandler(server)
 	testCaseResultHandler := handlers.NewTestCaseResultHandler(server)
 	userConnectionHandler := handlers.NewuserConnectionHandler(server)
-
 
 	server.Echo.GET("/swagger/*", echoSwagger.WrapHandler)
 	server.Echo.Use(middleware.Logger())
@@ -22,10 +21,10 @@ func ConfigureRoutes(server *s.Server){
 
 	apiGroup := server.Echo.Group("/subscribe")
 
-	apiGroup.GET("/chapter-permission/:group_id",chapterPermissionHandler.ConsumeChapterPermission)
-	apiGroup.GET("/class-log/:group_id",classLogHandler.ConsumeClassLog)
-	apiGroup.GET("/online-students/:group_id",onlineStudentsOldHandler.ConsumeOnlineStudentOld)
-	apiGroup.GET("/submission-result/:job_id",submitionResultHandler.ConsumeSubmitionResult)
-	apiGroup.GET("/testcase-result/:job_id",testCaseResultHandler.ConsumeTestCaseResult)
-	apiGroup.GET("/user/connection/:group_id",userConnectionHandler.ConsumeUserConnection)
+	apiGroup.GET("/group-permission/:group_id", groupPermissionHandler.ConsumeGroupPermission)
+	apiGroup.GET("/class-log/:group_id", classLogHandler.ConsumeClassLog)
+	apiGroup.GET("/online-students/:group_id", onlineStudentsOldHandler.ConsumeOnlineStudentOld)
+	apiGroup.GET("/submission-result/:job_id", submitionResultHandler.ConsumeSubmitionResult)
+	apiGroup.GET("/testcase-result/:job_id", testCaseResultHandler.ConsumeTestCaseResult)
+	apiGroup.GET("/user/connection/:user_id", userConnectionHandler.ConsumeUserConnection)
 }
