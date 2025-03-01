8a2eb92789cf98f407104abf6c91ab8d6fd7f1ac
Sirasit Thiancharoenchai <ootcazy007@gmail.com>
2025-03-01 23:53:03 +0700

    🧪 [APP-0008] Test sync and detect disconnection

--- server/handlers/user_connection_handle.go
+++ server/handlers/user_connection_handle.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/labstack/echo/v4"
 
@@ -30,6 +31,7 @@ type UserConnection struct {
 
 var (
 	userConnections = make(map[string]*UserConnection)
+	mu sync.Mutex
 )
 
 func NewuserConnectionHandler(server *s.Server) *userConnectionHandler {
@@ -57,6 +59,7 @@ func (userConnectionHanuserConnectionHandler *userConnectionHandler) ConsumeUser
 		isStudent = true
 	}
 
+	mu.Lock()
 	conn, exists := userConnections[userID]
 	if !exists {
 		conn = &UserConnection{
@@ -82,6 +85,7 @@ func (userConnectionHanuserConnectionHandler *userConnectionHandler) ConsumeUser
 	} else {
 		conn.TabCount++
 	}
+	mu.Unlock()
 
 	c.Response().Header().Set(echo.HeaderContentType, "text/event-stream")
 	c.Response().Header().Set(echo.HeaderCacheControl, "no-cache")
@@ -137,6 +141,7 @@ func (userConnectionHanuserConnectionHandler *userConnectionHandler) ConsumeUser
 
 	<-ctx.Done()
 
+	mu.Lock()
 	conn.TabCount--
 	if conn.TabCount <= 0 {
 		delete(userConnections, userID)
@@ -157,6 +162,7 @@ func (userConnectionHanuserConnectionHandler *userConnectionHandler) ConsumeUser
 			}
 		}
 	}
+	mu.Unlock()
 
 	subscriber.Unsubscribe(context.Background())
 	subscriber.Close()
